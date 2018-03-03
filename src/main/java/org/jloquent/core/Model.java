9f2abdbd6ecbdd3cbdcca98d0715ffee0a25977c
derickfelix <derickfelix4321@gmail.com>
2018-03-03 14:11:45 -0500

    refactor(project): add project as maven

---
+++ src/main/java/org/jloquent/core/Model.java
@@ -0,0 +1,221 @@
+/*
+ * The MIT License
+ *
+ * Copyright 2018 Derick Felix.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package org.jloquent.core;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Supplier;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author derickfelix
+ * @date Feb 24, 2018
+ */
+public abstract class Model {
+
+    /**
+     * Creates a new entity into a table with the same name of a model child but
+     * in plural, e.g. a model <code>class Person extends Model</code> will have
+     * all of its fields persisted into a table called <code>persons</code>.
+     */
+    public void save() {
+        Method[] mt = this.getClass().getDeclaredMethods();
+        List<Field> fields = ObjectUtility.getFields(mt, this, false);
+
+        String sql = "INSERT INTO `" + ObjectUtility.tableOf(this) + "` (";
+        for (int i = 0; i < fields.size(); i++) {
+            Field field = fields.get(i);
+
+            sql += ("`" + field.getName() + "`");
+
+            if ((i + 1) != fields.size()) {
+                sql += ", ";
+            }
+        }
+
+        sql += ") VALUES (";
+        for (int i = 0; i < fields.size(); i++) {
+            Field field = fields.get(i);
+
+            sql += ("'" + field.getValue() + "'");
+
+            if ((i + 1) != fields.size()) {
+                sql += ", ";
+            }
+        }
+
+        sql += ")";
+
+        Database.execute(sql);
+        System.out.println(sql);
+    }
+
+    /**
+     * Updates an entity in a table with the same name of a model child but in
+     * plural, e.g. a model <code>class Person extends Model</code> will have
+     * all of its fields updated, in a table called <code>persons</code>.
+     */
+    public void update() {
+        Method[] mt = this.getClass().getDeclaredMethods();
+        List<Field> fields = ObjectUtility.getFields(mt, this, false);
+        Object id = null;
+
+        String sql = "UPDATE `" + ObjectUtility.tableOf(this) + "` SET ";
+        for (int i = 0; i < fields.size(); i++) {
+            Field field = fields.get(i);
+            if (!field.getName().equals("id")) {
+                sql += "`" + field.getName() + "` = '" + field.getValue() + "'";
+
+                if ((i + 1) != fields.size()) {
+                    sql += ", ";
+                }
+            } else {
+                id = field.getValue();
+            }
+        }
+
+        if (id == null) {
+            System.out.println("id cannot be null");
+            return;
+        }
+
+        sql += " WHERE `id` = " + id;
+
+        Database.execute(sql);
+        System.out.println(sql);
+    }
+
+    public void delete() {
+        Method[] mt = this.getClass().getDeclaredMethods();
+        List<Field> fields = ObjectUtility.getFields(mt, this, false);
+        Object id = null;
+        String sql = "DELETE FROM `" + ObjectUtility.tableOf(this) + "`";
+
+        for (Field f : fields) {
+            if (f.getName().equals("id")) {
+                id = f.getValue();
+                break;
+            }
+        }
+
+        if (id == null) {
+            System.err.println("id cannot be null");
+            return;
+        }
+
+        sql += " WHERE `id` = " + id;
+
+        Database.execute(sql);
+        System.out.println(sql);
+    }
+
+    public static void create(Model model) {
+        model.save();
+    }
+
+    public static void update(Model model) {
+        model.update();
+    }
+
+    public static <M extends Model> M find(int id, Supplier<M> constructor) {
+        M instance = constructor.get();
+        Method[] methods = instance.getClass().getDeclaredMethods();
+        List<Method> setters = new ArrayList<>();
+        List<Field> fields = ObjectUtility.getFields(methods, instance, true);
+
+        for (Method m : methods) {
+            if (m.getName().contains("set")) {
+                setters.add(m);
+            }
+        }
+
+        String sql = "SELECT * FROM `" + ObjectUtility.tableOf(instance) + "` where `id` = " + id;
+
+        try {
+            ResultSet rs = Database.executeQuery(sql);
+
+            while (rs.next()) {
+                for (int i = 0; i < setters.size(); i++) {
+                    Method tempMethod = setters.get(i);
+                    for (int j = 0; j < fields.size(); j++) {
+                        Field tempField = fields.get(j);
+                        if (tempMethod.getName().toLowerCase().contains(tempField.getName())) {
+                            tempMethod.invoke(instance, Database.getResult(rs, tempField.getType(), tempField.getName()));
+                        }
+                    }
+                }
+            }
+        } catch (SQLException | IllegalAccessException | IllegalArgumentException | InvocationTargetException e) {
+            Logger.getLogger(Model.class.getName()).log(Level.SEVERE, "Failed execute query", e);
+        }
+        System.out.println(sql);
+        
+        return instance;
+    }
+
+    public static <M extends Model> List<M> all(Supplier<M> constructor) {
+        M instance = constructor.get();
+        List<M> models = new ArrayList<>();
+        Method[] methods = instance.getClass().getDeclaredMethods();
+        List<Method> setters = new ArrayList<>();
+        List<Field> fields = ObjectUtility.getFields(methods, instance, true);
+
+        for (Method m : methods) {
+            if (m.getName().contains("set")) {
+                setters.add(m);
+            }
+        }
+        String sql = "SELECT * FROM `" + ObjectUtility.tableOf(instance) + "`";
+        try {
+            ResultSet rs = Database.executeQuery(sql);
+
+            while (rs.next()) {
+                M model = constructor.get();
+
+                for (int i = 0; i < setters.size(); i++) {
+                    Method tempMethod = setters.get(i);
+                    for (int j = 0; j < fields.size(); j++) {
+                        Field tempField = fields.get(j);
+                        if (tempMethod.getName().toLowerCase().contains(tempField.getName())) {
+                            tempMethod.invoke(model, Database.getResult(rs, tempField.getType(), tempField.getName()));
+                        }
+                    }
+                }
+                models.add(model);
+            }
+        } catch (SQLException | IllegalAccessException | IllegalArgumentException | InvocationTargetException e) {
+            Logger.getLogger(Model.class.getName()).log(Level.SEVERE, "Failed to execute query", e);
+        }
+        System.out.println(sql);
+
+        return models;
+    }
+
+}
---
+++ src/main/java/org/jloquent/core/ObjectUtility.java
@@ -0,0 +1,89 @@
+/*
+ * The MIT License
+ *
+ * Copyright 2018 Derick Felix.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package org.jloquent.core;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author derickfelix
+ * @date Feb 24, 2018
+ */
+public class ObjectUtility {
+
+    public static String tableOf(Object obj) {
+        return obj.getClass().getSimpleName().toLowerCase() + "s";
+    }
+
+    /**
+     * Gets a list of fields containing a name and its value
+     *
+     * @param methods a array of methods of a object
+     * @param invoker the object that is invoking each method
+     * @param request whether it is request mode or not, if false, id field and
+     * fields with null values will not be included
+     * @return a list of fields
+     */
+    public static List<Field> getFields(Method[] methods, Object invoker, boolean request) {
+        List<Field> fields = new ArrayList<>();
+        try {
+            for (int i = methods.length - 1; i >= 0; i--) {
+                if (methods[i].getName().contains("get")) {
+                    String name = toFieldName(methods[i].getName());
+                    Object value = methods[i].invoke(invoker, (Object[]) null);
+                    Class<?> type = methods[i].getReturnType();
+                    
+                    if (request || (!name.contains("id") && value != null)) {
+                        fields.add(new Field(name, value, type.getSimpleName()));
+                    }
+                }
+            }
+        } catch (IllegalArgumentException | IllegalAccessException | InvocationTargetException e) {
+            Logger.getLogger(ObjectUtility.class.getName()).log(Level.SEVERE, "Failed to get name of fields", e);
+        }
+
+        return fields;
+    }
+
+    /**
+     * Removes the 'get' prefix of string and set its first character to lower
+     * case, for example a string with <code>getAddress</code> as its value will
+     * return <code>address</code>.
+     *
+     * @param getterMethod a string with the 'get' prefix.
+     * @return a string without the 'get' prefix and its first character to
+     * lower case.
+     */
+    private static String toFieldName(String getterMethod) {
+        // eliminate get** Prefix
+        char[] field = getterMethod.substring(3).toCharArray();
+        field[0] = getterMethod.toLowerCase().charAt(3);
+        return new String(field);
+    }
+}
